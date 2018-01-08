5369a1e196501f86d1d361547e6180cf99500ce7
haripermadi <harinurpermadi@gmail.com>
2018-01-08 15:46:21 +0700

    pseudocode

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+loop tanggal dari 7 - 31
+set jadwal isinya tanggal + hasil loop i
+set nama dengan array kosong
+jika i habis dibagi 5 maka tambah ke jadwal 'tempat fitness tutup'
+else
+    set j ke 0
+    jika tanggal habis dibagi 2 maka tambah Tono
+    jika tanggal habis dibagi 4 maka tambah Anton
+    jika tanggal habis dibawgi 5 maka tambah Budi
+munculkan semua
+
+*/
+
