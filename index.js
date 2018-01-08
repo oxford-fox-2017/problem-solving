87d748b30de2e3bb6a006a38cb77444838788925
Tobias Kusnaman <tobiaskusnaman@Tobiass-MacBook-Pro.local>
2018-01-08 17:07:01 +0700

    tambah pseudocode

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+/*
+1. buat variable object buat pelanggan dan hari dia masuk
+2. looping dimulai dr tanggal 7 dan berakhir di tanggal 31
+3. kalo kelipatan 5 berarti tempat tutup,
+4. else kalo (hari-7) modulus dari hari orang == 0 maka push ke array
+5. print tanggal dan array
+*/
+
 let obj = [{
-  nama : 'Tono',
-  hari : 2
-}, {
-  nama : 'Anton',
-  hari : 4
-}, {
-  nama : 'Budi',
-  hari : 5
-}
+    nama : 'Tono',
+    hari : 2
+  }, {
+    nama : 'Anton',
+    hari : 4
+  }, {
+    nama : 'Budi',
+    hari : 5
+  }
 ]
 
 for (var i = 7; i <= (31); i++) {
@@ -23,14 +31,3 @@ for (var i = 7; i <= (31); i++) {
   }
   console.log(`Tanggal ${i}: ${pelanggan}`);
 }
-
-
-// /*
-// Tanggal 7: Tono, Anton, Budi
-// Tanggal 8:
-// Tanggal 9: Tono
-// Tanggal 10: Tempat Fitness Tutup
-// Tono, 2 hari sekali
-// Anton, 4 hari sekali
-// budi, 5 hari sekali
-// */
