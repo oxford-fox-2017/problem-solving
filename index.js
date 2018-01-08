6d2f7106ea133faee6b44f0c22b6c9b566c1dda3
haripermadi <harinurpermadi@gmail.com>
2018-01-08 15:52:00 +0700

    update pseudo

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ set jadwal isinya tanggal + hasil loop i
 set nama dengan array kosong
 jika i habis dibagi 5 maka tambah ke jadwal 'tempat fitness tutup'
 else
-    set j ke 0
-    jika tanggal habis dibagi 2 maka tambah Tono
-    jika tanggal habis dibagi 4 maka tambah Anton
-    jika tanggal habis dibawgi 5 maka tambah Budi
+    set start ke 0 (i-7) tanggal 7 ==0, 8=1, 9=2,10=3,11=4,12=5,dst
+    jika start habis dibagi 2 maka tambah Tono
+    jika start habis dibagi 4 maka tambah Anton
+    jika start habis dibawgi 5 maka tambah Budi
 munculkan semua
 
 */
