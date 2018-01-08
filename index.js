b89f3db8f1b28d2fe1b0a16de83f09007f3208f4
Robert <dominicusrobert@yahoo.com>
2018-01-08 15:49:18 +0700

    done

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // 1. Set nilai "tanggal_hari_ini" dengan nilai 7
 // 2. Perulangan :
-//     Buat baru variabel person;
+//     Jika "tanggal_hari_ini" lebih besar dari 31, keluar dari Perulangan
+
+//     Set nilai "person" dengan nilai ""
 //     Jika "tanggal_hari_ini" sama dengan 7, tambah variabel person dengan "Tono, Anton, Budi, "
-//     Jika "tanggal_hari_ini" sama dengan atau lebih besar dari 31, keluar dari Perulangan
-
-//     Jika sisa bagi dari "tanggal_hari_ini" dibagi 2 sama dengan 0, maka CETAK "Tono, "
-//     Jika sisa bagi dari "tanggal_hari_ini" dibagi 4 sama dengan 0, maka CETAK "Anton, "
-//     Jika sisa bagi dari "tanggal_hari_ini" dibagi 5 sama dengan 0, maka CETAK "Budi, "
-
+//     Jika "tanggal_hari_ini" sama dengan 10, tambah variabel person dengan "Tempat fitnes tutup"
+//     Jika "tanggal_hari_ini" bukan sama dengan 7 atau 10
+//          Jika sisa bagi dari ("tanggal_hari_ini" dikurang 7) dibagi 2 sama dengan 0, maka CETAK "Tono, "
+//          Jika sisa bagi dari ("tanggal_hari_ini" dikurang 7) dibagi 4 sama dengan 0, maka CETAK "Anton, "
+//          Jika sisa bagi dari ("tanggal_hari_ini" dikurang 7) dibagi 5 sama dengan 0, maka CETAK "Budi, "
+//     CETAK "tanggal_hari_ini" dengan "person"
 //     TAMBAH NILAI +1 pada "tanggal_hari_ini"
-
-//     CETAK "tanggal_hari_ini"
 
 
 for(var i=7; i<=31; i++){
