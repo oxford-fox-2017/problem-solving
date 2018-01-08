f8a9e270465ac0aefe5d0a626df3d1be97c37a54
aldiws <syldie.aldiwijaya@gmail.com>
2018-01-08 19:51:00 +0700

    add-pseudocode

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+// Inisialisasi variabel jadwal = array of object yang berisi nama dan waktu
+// Inisialisasi variabel bulan = 31
+// Inisialisasi variabel buka = 7
+// Loop dari 'buka' sampai 'bulan'
+// jika 'i' habis dibagi 5 sama dengan 0 maka tampilkan Tempat Fitnes Libur
+// selain itu 
+// Inisialisasi variabel result = []
+// lakukan loop untuk mendapatkan value dari 'jadwal'
+// jika 'i-7' habis dibagi 'jadwal.waktu' sama dengan 0
+// maka push 'jadwal.nama' kedalam 'result'
+// tampilkan tanggal i 'result'
+
 function problem_solving(input) {
   let bulan = 31
   let buka = 7
   for (let i = buka; i <= bulan; i++) {
-    if (i === buka) {
-      console.log('Tanggal :' + i + ' Tono, Anton, Budi')
-    } else if (i % 5 === 0) {
+    if (i % 5 === 0) {
       console.log('Tanggal :' + i + ' Tempat Fitnes Libur')
     } else {
       let result = []
